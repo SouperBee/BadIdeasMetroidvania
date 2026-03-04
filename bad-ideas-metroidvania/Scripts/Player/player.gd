592290c4b799ee4c2354438cd2fddb6025724b63
SouperBee <cochranejarek03@outlook.com>
2026-03-03 22:03:54 -0700

    Some Stuff

--- bad-ideas-metroidvania/Scripts/Player/player.gd
+++ bad-ideas-metroidvania/Scripts/Player/player.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
 
+# Node References
+@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
+
+#Constants
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -520.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction < 0:
+			player_sprite.scale = Vector2(1.0, -1.0)
+			player_sprite.rotation_degrees = 180.0
+		elif direction > 0:
+			player_sprite.scale = Vector2(1.0, 1.0)
+			player_sprite.rotation_degrees = 0.0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
